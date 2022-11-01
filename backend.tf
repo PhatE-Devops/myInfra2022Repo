ed2e62d438ee9ccc63538214742bc20fb312ca3c
PhatE-Devops <114031855+PhatE-Devops@users.noreply.github.com>
2022-11-01 17:42:28 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-tf-state-bucket"
+    bucket = "my-dev-tf-state-bucket22"
     key = "main"
     region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
